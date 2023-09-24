1df74fe1ed8504e42eda61a50cf1546c06e58261
yampolskyi10 <bizi34q@gmail.com>
2023-09-24 16:32:14 +0200

    add

---
+++ src/js/task-02.js
@@ -0,0 +1,31 @@
+
+const ingredientsList = document.querySelector('#ingredients');
+
+
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatoes",
+  "Herbs",
+  "Condiments",
+];
+
+
+const fragment = document.createDocumentFragment();
+
+
+ingredients.forEach((ingredient) => {
+ 
+  const listItem = document.createElement('li');
+  
+
+  listItem.textContent = ingredient;
+  
+  
+  listItem.classList.add('item');
+  
+  fragment.appendChild(listItem);
+});
+
+ingredientsList.appendChild(fragment);
